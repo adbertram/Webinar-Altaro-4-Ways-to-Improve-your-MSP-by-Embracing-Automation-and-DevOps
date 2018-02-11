c1e6cfd16ecc6e18e1fa05fdc89025ff4a59d07a
Adam Bertram <adbertram@gmail.com>
2018-02-11 17:54:22 -0600

    Updates

--- Demo 1/SoftwareDeployment/Deploy-VNC.ps1
+++ Demo 1/SoftwareDeployment/Deploy-VNC.ps1
@@ -1,6 +1,4 @@
-#Requires -Version 4
-function Deploy-Vnc {
-	<#
+<#
 	.SYNOPSIS
 		This function deploys the UltraVNC software package to a remote computer.
 		
@@ -20,54 +18,53 @@ function Deploy-Vnc {
 	.PARAMETER Credential
 		A PSCredential object representing alternate username and password to connect to the remote computer.
 	#>
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory)]
-		[ValidateNotNullOrEmpty()]
-		[string[]]$ComputerName,
+[CmdletBinding()]
+param
+(
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string[]]$ComputerName,
 		
-		[Parameter(Mandatory)]
-		[ValidateNotNullOrEmpty()]
-		[ValidateScript({ Test-Path -Path $_ -PathType Container })]
-		[string]$InstallerFolderPath,
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[ValidateScript({ Test-Path -Path $_ -PathType Container })]
+	[string]$InstallerFolderPath,
 
-		[Parameter(Mandatory)]
-		[pscredential]$Credential
-	)
+	[Parameter(Mandatory)]
+	[pscredential]$Credential
+)
 	
-	$installFolderName = $InstallerFolderPath.Split('\')[-1]
+$installFolderName = $InstallerFolderPath.Split('\')[-1]
 	
-	foreach ($c in $ComputerName) {
-		try {
-			$jobBlock = {
-				$uncInstallerFolder = "\\$c\c$\$installFolderName"
-				Copy-Item -Path $InstallerFolderPath -Destination "\\$c\c$" -Recurse
+foreach ($c in $ComputerName) {
+	try {
+		$jobBlock = {
+			$uncInstallerFolder = "\\$c\c$\$installFolderName"
+			Copy-Item -Path $InstallerFolderPath -Destination "\\$c\c$" -Recurse
 					
-				$scriptBlock = { 
-					$VerbosePreference = $using:VerbosePreference
+			$scriptBlock = { 
+				$VerbosePreference = $using:VerbosePreference
 					
-					## Remotely invoke the VNC installer on the computer
-					$localInstallFolder = "C:\$using:installFolderName".TrimEnd('\')
-					$localInstaller = "$localInstallFolder\Setup.exe"
-					$localInfFile = "$localInstallFolder\silentnstall.inf"
+				## Remotely invoke the VNC installer on the computer
+				$localInstallFolder = "C:\$using:installFolderName".TrimEnd('\')
+				$localInstaller = "$localInstallFolder\Setup.exe"
+				$localInfFile = "$localInstallFolder\silentnstall.inf"
 
-					Start-Process $using:localInstaller -Args "/verysilent /loadinf=`"$using:localInfFile`"" -Wait -NoNewWindow
-				}
-				Invoke-Command -ComputerName $c -ScriptBlock $scriptBlock -Credential $Credential
+				Start-Process $using:localInstaller -Args "/verysilent /loadinf=`"$using:localInfFile`"" -Wait -NoNewWindow
 			}
-			$jobs = Start-Job -ScriptBlock $jobBlock
-			
-		} catch {
-			$PSCmdlet.ThrowTerminatingError($_)
-		} finally {
-			$remoteInstallFolder = "\\$c\c$\$installFolderName"
-			Write-Verbose -Message "Cleaning up VNC install bits at [$($remoteInstallFolder)]"
-			Remove-Item $remoteInstallFolder -Recurse -ErrorAction Ignore
+			Invoke-Command -ComputerName $c -ScriptBlock $scriptBlock -Credential $Credential
 		}
+		$jobs = Start-Job -ScriptBlock $jobBlock
+			
+	} catch {
+		$PSCmdlet.ThrowTerminatingError($_)
+	} finally {
+		$remoteInstallFolder = "\\$c\c$\$installFolderName"
+		Write-Verbose -Message "Cleaning up VNC install bits at [$($remoteInstallFolder)]"
+		Remove-Item $remoteInstallFolder -Recurse -ErrorAction Ignore
 	}
-	while ($jobs | Where-Object { $_.State -eq 'Running'}) {
-		Write-Verbose -Message "Waiting for all computers to finish..."
-		Start-Sleep -Second 1
-	}
+}
+while ($jobs | Where-Object { $_.State -eq 'Running'}) {
+	Write-Verbose -Message "Waiting for all computers to finish..."
+	Start-Sleep -Second 1
 }
